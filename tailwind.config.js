090f0a8338e4cbd80c6920f11c4b14984903276b
sisira-neha <158795217+sisira-neha@users.noreply.github.com>
2025-06-22 20:23:22 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        professional: ["'Inter'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
